107384dd4beea98a677ee52cfd650c0c7e88d9ab
sabarn0 <sabarno99@gmail.com>
2023-11-21 01:45:53 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 import React, {UseState, UseEffect} from 'react';
 import MessageComponent from './message.js';
+import logo from './logo.svg'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        
+        <img src={logo} className="App-logo" alt="logo" />
+{/*         
         <a className="App-link">
-        <LiveClockUpdate></LiveClockUpdate>
-        </a> */}
+        <LiveClockUpdate></LiveClockUpdate> */}
         
         <a className='App-link'>
-          
-      <MessageComponent />
+          <App />
+      {/* <MessageComponent /> */}
         </a>
       </header>
     </div>
