63fcbae4bd2fbe7a1a5b1ffb25456f7f686133a0
sabarn0 <sabarno99@gmail.com>
2023-11-23 03:12:29 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
-// Retrieve the application usage count from localStorage
 const appUsageCount = parseInt(localStorage.getItem('appUsageCount')) || 0;
 
 if(appUsageCount){
   localStorage.setItem('appUsageCount', parseInt(appUsageCount) +1)
 }
 else{
-  localStorage.setItem('appUsageCount', parseInt(1))
+  localStorage.setItem('appUsageCount', 1)
 }
 
-// Check if the count is a multiple of 5 and show an alert
 if (appUsageCount > 0 && appUsageCount % 5 === 0) {
   alert("If you miss him you should text him..")
 }
@@ -26,7 +24,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
